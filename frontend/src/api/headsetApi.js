469bd44a9ac60a3e6d87fdf120a62becec3616be
Kouthar Davids <kouthardavids606@gmail.com>
2025-09-05 12:47:18 +0200

    Updated my API's

--- frontend/src/api/headsetApi.js
+++ frontend/src/api/headsetApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getAccessToken, clearAccessToken, setAccessToken } from "./authToken.js";
 
-export const API_BASE_URL = 'http://localhost:5006/api'
+export const API_BASE_URL = `${process.env.REACT_APP_BACKEND_URL}/api`;
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
