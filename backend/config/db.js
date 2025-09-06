eb9ef9514565122c62e76914d6b0bc272dfe7790
Kouthar Davids <kouthardavids606@gmail.com>
2025-09-06 08:55:46 +0200

    fixed db file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,17 @@
-import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
+
 dotenv.config();
 
-async function testDB() {
-  try {
-    const conn = await mysql.createConnection({
-      host: process.env.MYSQL_HOST,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
-      port: process.env.MYSQL_PORT,
-    });
-    console.log('DB Connected!');
-    await conn.end();
-  } catch (err) {
-    console.error('DB Connection Failed:', err);
-  }
-}
+const db = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
-testDB();
+export default db;
